12bb9ddc97d86dceeb93f52fdbaeb4385feb1bb1
DevayaniCh <devayani6300@gmail.com>
2023-09-30 21:26:26 +0530

    onlineBanking struct

---
+++ src/com/marolix/banking/BankDetails.java
@@ -0,0 +1,13 @@
+package com.marolix.banking;
+
+import java.util.Scanner;
+
+public class BankDetails 
+{
+public static void main(String[] args) {
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter the User Full Name");
+	String name=scanner.nextLine();
+
+}
+}
